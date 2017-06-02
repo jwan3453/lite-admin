028bc822f236584245579bd4498c7e87e3f7a503
Milo.Hou <knightuniverse@qq.com>
2017-06-02 15:21:09 +0800

    学生列表页面 -> 学生信息弹窗，基础信息表单，以及基础信息修改弹窗

--- src/components/Common/StudentInfo/basicInfo.js
+++ src/components/Common/StudentInfo/basicInfo.js
@@ -4,8 +4,15 @@ import {
   Row,
   Col,
   Spin,
+  Button,
+  Tooltip,
+  Popconfirm,
+  Modal,
 } from 'antd';
 import moment from 'moment';
+import _ from 'lodash';
+import crmStatus from '../../../common/crmStatus';
+import BasicProfileForm from './basicProfileForm';
 
 class StudentBasicInfo extends Component {
   static propTypes = {
@@ -16,6 +23,19 @@ class StudentBasicInfo extends Component {
   static defaultProps = {
     loading: false,
     studentInfo: {},
+  };
+
+  state = {
+    giftDialogVisible: false,
+    profileDialogVisible: false,
+  };
+
+  handleChangeProfile = () => {
+    // todo update user profile
+  };
+
+  handlePasswordResetConfirm = () => {
+    // todo reset user password
   };
 
   render() {
@@ -34,45 +54,94 @@ class StudentBasicInfo extends Component {
       span: 5,
     };
 
+    const currentCrmStatus = _.find(crmStatus, item => item.value === studentInfo.crm.status);
+
     return (
-      <Spin spinning={loading}>
-        <Row style={{ lineHeight: '30px', padding: '0 16px' }}>
-          <Col {...labelProps}>用户编号：</Col>
-          <Col {...contentProps}>{studentInfo.id}</Col>
-          <Col {...labelProps}>昵称：</Col>
-          <Col {...contentProps}>{studentInfo.nickname || '--'}</Col>
-          <Col {...labelProps}>性别：</Col>
-          <Col {...contentProps}>{studentInfo.gender || '--'}</Col>
-          <Col {...labelProps}>手机号码：</Col>
-          <Col {...contentProps}>{studentInfo.phone || '--'}</Col>
-          <Col {...labelProps}>所属组：</Col>
-          <Col {...contentProps}>{studentInfo.group || '--'}</Col>
-          <Col {...labelProps}>出生日期：</Col>
-          <Col {...contentProps}>{studentInfo.birthDay ? moment(new Date(studentInfo.birthDay)).format('YYYY-MM-DD') : '--'}</Col>
-          <Col {...labelProps}>所在地区：</Col>
-          <Col {...contentProps}>{studentInfo.contact.address || '--'}</Col>
-          <Col {...labelProps}>所属助教：</Col>
-          <Col {...contentProps}>{studentInfo.assistant.nickname}</Col>
-          <Col {...labelProps}>CRM状态：</Col>
-          <Col {...contentProps}>{studentInfo.crm.status}</Col>
-          <Col {...labelProps}>来源渠道：</Col>
-          <Col {...contentProps}>{studentInfo.funnel || '--'}</Col>
-          <Col {...labelProps}>课程级别：</Col>
-          <Col {...contentProps}>{studentInfo.level || '--'}</Col>
-          <Col {...labelProps}>沟通类型：</Col>
-          <Col {...contentProps}>{studentInfo.contact.type || '--'}</Col>
-          <Col {...labelProps}>联系IM：</Col>
-          <Col {...contentProps}>{studentInfo.contact.im || '--'}</Col>
-          <Col {...labelProps}>累计购买：</Col>
-          <Col {...contentProps}>{studentInfo.hours.sum.toFixed(1)}</Col>
-          <Col {...labelProps}>剩余可用：</Col>
-          <Col {...contentProps}>{studentInfo.hours.available.toFixed(1)}</Col>
-          <Col {...labelProps}>总奖学金：</Col>
-          <Col {...contentProps}>{studentInfo.scholarship.sum}</Col>
-          <Col {...labelProps}>可用奖学金：</Col>
-          <Col {...contentProps}>{studentInfo.scholarship.available}</Col>
-        </Row>
-      </Spin>
+      <div>
+        <Spin spinning={loading}>
+          <Row style={{ lineHeight: '30px', padding: '0 16px' }}>
+            <Col {...labelProps}>用户编号：</Col>
+            <Col {...contentProps}>{studentInfo.id}
+              <Popconfirm
+                title="操作不可逆，确定继续？"
+                onConfirm={this.handlePasswordResetConfirm}
+              >
+                <Tooltip title="重置密码">
+                  <Button
+                    icon="lock"
+                    size="small"
+                    style={{ marginLeft: 5 }}
+                  />
+                </Tooltip>
+              </Popconfirm>
+              <Tooltip title="修改信息">
+                <Button
+                  icon="edit"
+                  size="small"
+                  style={{ marginLeft: 5 }}
+                  onClick={() => this.setState({ profileDialogVisible: true })}
+                />
+              </Tooltip>
+            </Col>
+            <Col {...labelProps}>昵称：</Col>
+            <Col {...contentProps}>{studentInfo.nickname || '--'}</Col>
+            <Col {...labelProps}>性别：</Col>
+            <Col {...contentProps}>{studentInfo.gender || '--'}</Col>
+            <Col {...labelProps}>手机号码：</Col>
+            <Col {...contentProps}>{studentInfo.phone || '--'}</Col>
+            <Col {...labelProps}>所属组：</Col>
+            <Col {...contentProps}>{studentInfo.group || '--'}</Col>
+            <Col {...labelProps}>出生日期：</Col>
+            <Col {...contentProps}>{studentInfo.birthDay ? moment(new Date(studentInfo.birthDay)).format('YYYY-MM-DD') : '--'}</Col>
+            <Col {...labelProps}>所在地区：</Col>
+            <Col {...contentProps}>{studentInfo.contact.address || '--'}</Col>
+            <Col {...labelProps}>所属助教：</Col>
+            <Col {...contentProps}>{studentInfo.assistant.nickname}</Col>
+            <Col {...labelProps}>CRM状态：</Col>
+            <Col {...contentProps}>{currentCrmStatus.name}</Col>
+            <Col {...labelProps}>来源渠道：</Col>
+            <Col {...contentProps}>{studentInfo.funnel || '--'}</Col>
+            <Col {...labelProps}>课程级别：</Col>
+            <Col {...contentProps}>{studentInfo.level || '--'}</Col>
+            <Col {...labelProps}>沟通类型：</Col>
+            <Col {...contentProps}>{studentInfo.contact.type || '--'}</Col>
+            <Col {...labelProps}>联系IM：</Col>
+            <Col {...contentProps}>{studentInfo.contact.im || '--'}</Col>
+            <Col {...labelProps}>累计购买：</Col>
+            <Col {...contentProps}>{studentInfo.hours.sum.toFixed(1)}</Col>
+            <Col {...labelProps}>剩余可用：</Col>
+            <Col {...contentProps}>{studentInfo.hours.available.toFixed(1)}
+              <Tooltip
+                title="赠送课时"
+              >
+                <Button
+                  icon="credit-card"
+                  style={{ marginLeft: 5 }}
+                  onClick={() => this.setState({ giftDialogVisible: true })}
+                />
+              </Tooltip>
+            </Col>
+            <Col {...labelProps}>总奖学金：</Col>
+            <Col {...contentProps}>{studentInfo.scholarship.sum}</Col>
+            <Col {...labelProps}>可用奖学金：</Col>
+            <Col {...contentProps}>{studentInfo.scholarship.available}</Col>
+          </Row>
+        </Spin>
+        <Modal
+          title="修改信息"
+          visible={this.state.profileDialogVisible}
+          onOk={this.handleChangeProfile}
+          onCancel={() => this.setState({ profileDialogVisible: false })}
+        >
+          <BasicProfileForm />
+        </Modal>
+        <Modal
+          title="赠送课程"
+          visible={this.state.giftDialogVisible}
+          onOk={this.handleSendGift}
+          onCancel={() => this.setState({ giftDialogVisible: false })}
+        >gift form here</Modal>
+      </div>
     );
   }
 }
---
+++ src/components/Common/StudentInfo/basicProfileForm.js
@@ -0,0 +1,151 @@
+import React, { Component } from 'react';
+import {
+  Form,
+  Input,
+  Radio,
+  DatePicker,
+  Select,
+} from 'antd';
+
+import levels from '../../../common/levels';
+
+const RadioGroup = Radio.Group;
+const FormItem = Form.Item;
+
+class BasicProfileForm extends Component {
+  static propTypes = {
+    form: React.PropTypes.object.isRequired,
+  };
+
+  static propTypes = {};
+
+  static defaultProps = {};
+
+  state = {
+  };
+
+  render() {
+    const formItemLayout = {
+      labelCol: { span: 7 },
+      wrapperCol: { span: 12 },
+    };
+
+    const { getFieldDecorator } = this.props.form;
+
+    return (
+      <Form>
+        <FormItem
+          label="昵称"
+          {...formItemLayout}
+        >
+          {
+            getFieldDecorator('nickname', {
+              rules: [
+                {
+                  required: false,
+                },
+              ],
+            })(<Input placeholder="用户昵称" />)
+          }
+        </FormItem>
+        <FormItem
+          label="性别"
+          {...formItemLayout}
+        >
+          {
+            getFieldDecorator('gender', {
+              rules: [
+                {
+                  required: false,
+                },
+              ],
+            })(
+              <RadioGroup>
+                <Radio value={1}>男</Radio>
+                <Radio value={2}>女</Radio>
+              </RadioGroup>,
+            )
+          }
+        </FormItem>
+        <FormItem
+          label="出生日期"
+          {...formItemLayout}
+        >
+          {
+            getFieldDecorator('birthDay', {
+              rules: [
+                {
+                  required: false,
+                },
+              ],
+            })(<DatePicker placeholder="请选择日期" />)
+          }
+        </FormItem>
+        <FormItem
+          label="课程级别"
+          {...formItemLayout}
+        >
+          {
+            getFieldDecorator('level', {
+              rules: [
+                {
+                  required: false,
+                },
+              ],
+            })(
+              <Select>
+                {
+                  levels.map(item => (
+                    <Select.Option key={item.value} value={item.value}>{item.name}</Select.Option>
+                  ))
+                }
+              </Select>,
+            )
+          }
+        </FormItem>
+        <FormItem
+          label="助教"
+          {...formItemLayout}
+        >
+          {
+            getFieldDecorator('assistant', {
+              rules: [
+                {
+                  required: false,
+                },
+              ],
+            })(
+              <Select>
+                <Select.Option key={1} value={1}>lgchen</Select.Option>
+                <Select.Option key={2} value={2}>linus</Select.Option>
+              </Select>,
+            )
+          }
+        </FormItem>
+        <FormItem
+          label="所属组"
+          {...formItemLayout}
+        >
+          {
+            getFieldDecorator('group', {
+              initialValue: -1,
+              rules: [
+                {
+                  required: false,
+                },
+              ],
+            })(
+              <Select>
+                <Select.Option key="-1" value="-1">全部</Select.Option>
+                <Select.Option key={1} value={1}>group 1</Select.Option>
+                <Select.Option key={2} value={2}>group 2</Select.Option>
+              </Select>,
+            )
+          }
+        </FormItem>
+      </Form>
+    );
+  }
+}
+
+export default Form.create()(BasicProfileForm);
